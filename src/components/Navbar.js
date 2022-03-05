90c639516c6f89c2646b44c67f6ad5e9bb776b21
vicsous <vicsous@protonmail.com>
2022-03-05 15:56:01 -0300

    Styling back button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,10 +3,14 @@ import { useHistory } from "react-router-dom";
 
 export default function Navbar({open, setOpen}) {
     let history = useHistory();
+    function handleBack() {
+        setOpen(!open)
+        history.goBack()
+    }
     return (
     <nav className="navbar">
-        <div className='menuBtn' onClick={() => history.goBack()}>
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+        <div className='menuBtn' onClick={handleBack}>
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                 <path strokeLinecap="round" strokeLinejoin="round" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
             </svg>
         </div>
