e9038fe716162fc37763a57d74dc69d8548fbf44
vicsous <vicsous@protonmail.com>
2022-03-05 15:45:58 -0300

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,5 +17,16 @@ export default function Navbar({open, setOpen}) {
         <div className='logo'>
             MiniBlog
         </div>
+        <div className='menuBtn' onClick={() => setOpen(!open)}>
+            {open?  <button><svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+                    </svg></button> :
+            <button className="space-y-2 pointer">
+                <div className="w-8 h-0.5 bg-white"></div>
+                <div className="w-8 h-0.5 bg-white"></div>
+                <div className="w-8 h-0.5 bg-white"></div>
+            </button>
+          }
+        </div>
     </nav>)
 }
