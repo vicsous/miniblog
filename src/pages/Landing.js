e6a8a9166269a542b2961d57f23cf631c98b504c
vicsous <vicsous@protonmail.com>
2022-03-05 14:23:50 -0300

    Styling landing page text

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -3,7 +3,10 @@ import "../styles/pages/Landing.css";
 export default function Landing() {
   return (
     <div className="landing">
-      <h1 className="landingTitle">Seja bem vindo!</h1>
+      <div className="landingText">
+        <h1 className="landingTitle">Seja bem vindo!</h1>
+        <h1 className="landingSubtitle">Entre ou cadastre-se em nossa rede.</h1>
+      </div>
     </div>
   );
 }
