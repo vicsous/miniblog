a4fbda903541b4d62a589ec4e917a6d24211cca8
vicsous <vicsous@protonmail.com>
2022-03-05 16:04:20 -0300

    Styling back arrow

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,13 +4,13 @@ import { useHistory } from "react-router-dom";
 export default function Navbar({open, setOpen}) {
     let history = useHistory();
     function handleBack() {
-        setOpen(!open)
+        setOpen(false)
         history.goBack()
     }
     return (
     <nav className="navbar">
         <div className='menuBtn' onClick={handleBack}>
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                 <path strokeLinecap="round" strokeLinejoin="round" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
             </svg>
         </div>
