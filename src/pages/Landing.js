195977db5cbffaa0e76dab1a615a10365cb53dde
vicsous <vicsous@protonmail.com>
2022-03-05 11:16:59 -0300

    Setting Landing page

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -3,8 +3,8 @@ import "../styles/pages/Landing.css";
 export default function Landing() {
   return (
     <div className="landing">
-      <div className="left"></div>
-      <div className="right"></div>
+      <div className="left">Left Side</div>
+      <div className="right">Right Side</div>
     </div>
   );
 }
