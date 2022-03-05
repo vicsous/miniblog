cf406ce526daa47d8d57a9be7302d09245dc4c17
vicsous <vicsous@protonmail.com>
2022-03-05 12:51:49 -0300

    Changing menu links

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -25,7 +25,7 @@ export default function Menu({open, setOpen}) {
                 </div>
             </Link>
 
-            <Link onClick={() => setOpen(!open)} to='/'>
+            <Link onClick={() => setOpen(!open)} to='/home'>
                 <button className="item">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
@@ -44,7 +44,7 @@ export default function Menu({open, setOpen}) {
                 </button>
             </Link>
 
-            <Link onClick={() => setOpen(!open)} to='/'>
+            <Link onClick={() => setOpen(!open)} to='/home'>
                 <button className="item newPost">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
@@ -72,7 +72,7 @@ export default function Menu({open, setOpen}) {
                 </button>
             </Link>
 
-            <Link onClick={handleLogout} to='/login'>
+            <Link onClick={handleLogout} to='/logout'>
                 <button className="item">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
