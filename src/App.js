79c432924b242ce37905e4aa53b2bd8114f02cff
vicsous <vicsous@protonmail.com>
2022-03-05 11:05:02 -0300

    Landing page created

--- src/App.js
+++ src/App.js
@@ -1,52 +1,56 @@
-import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Home from './pages/Home.js';
-import Login from './pages/Login.js';
-import Register from './pages/Register.js';
-import Friends from './pages/Friends.js';
-import Search from './pages/Search.js';
-import Settings from './pages/Settings.js';
-import Profile from './pages/Profile.js';
+import Home from "./pages/Home.js";
+import Login from "./pages/Login.js";
+import Register from "./pages/Register.js";
+import Friends from "./pages/Friends.js";
+import Search from "./pages/Search.js";
+import Settings from "./pages/Settings.js";
+import Profile from "./pages/Profile.js";
+import Landing from "./pages/Landing.js";
 
-import Header from './components/Header.js';
-import Footer from './components/Footer.js';
-import PrivateRoute from './components/PrivateRoute';
-import { AuthProvider } from './contexts/AuthContext';
+import Header from "./components/Header.js";
+import Footer from "./components/Footer.js";
+import PrivateRoute from "./components/PrivateRoute";
+import { AuthProvider } from "./contexts/AuthContext";
 
 export default function App() {
   return (
     <Router>
       <AuthProvider>
-      <Header />
-      <Switch> 
-        <PrivateRoute exact path='/' component={Home} />
-        <PrivateRoute path='/friends' component={Friends} />
-        <PrivateRoute path='/settings' component={Settings} />
+        <Header />
+        <Switch>
+          <PrivateRoute exact path="/home" component={Home} />
+          <PrivateRoute path="/friends" component={Friends} />
+          <PrivateRoute path="/settings" component={Settings} />
 
-        <Route path='/login'>
-          <Login />  
-        </Route>
+          <Route path="/">
+            <Landing />
+          </Route>
 
-        <Route path='/register'>
-          <Register />  
-        </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
 
-        <Route path='/search'>
-          <Search />  
-        </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
 
-        <Route path='/profile'>
-          <Profile />
-        </Route>
-        
-        <Route path="*">
-          <h1>Page not found</h1>
-        </Route>
+          <Route path="/search">
+            <Search />
+          </Route>
 
-      </Switch>
-      <Footer />
+          <Route path="/profile">
+            <Profile />
+          </Route>
+
+          <Route path="*">
+            <h1>Page not found</h1>
+          </Route>
+        </Switch>
+        <Footer />
       </AuthProvider>
     </Router>
-    );
+  );
 }
---
+++ src/pages/Landing.js
@@ -0,0 +1,10 @@
+import "../styles/pages/Landing.css";
+
+export default function Landing() {
+  return (
+    <div className="login">
+      <div className="left"></div>
+      <div className="right"></div>
+    </div>
+  );
+}
