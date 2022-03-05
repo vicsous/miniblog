3fd954544b8009067329a97dfee4e4db55d3dcbf
vicsous <vicsous@protonmail.com>
2022-03-05 14:27:43 -0300

    Styling landing page bg colors

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -7,6 +7,10 @@ export default function Landing() {
         <h1 className="landingTitle">Seja bem vindo!</h1>
         <h1 className="landingSubtitle">Entre ou cadastre-se em nossa rede.</h1>
       </div>
+      <div className="landingText">
+        <h1 className="landingTitle">Seja bem vindo!</h1>
+        <h1 className="landingSubtitle">Entre ou cadastre-se em nossa rede.</h1>
+      </div>
     </div>
   );
 }
